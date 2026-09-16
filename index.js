6608d40bb5fca8671963ffb54142cd1754c068b5
InspiredGH <solomon.asante62@gmail.com>
2026-09-16 13:09:56 +0000

    jquery update

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-$('h1').css('color', 'red')
+$('h1').addClass('heading margin')
 
+$('h1').click(function () {
+  console.log(this)
+  $(this).css('color', 'green')
+  $(this).css('border', 'red 2px solid')
+  $(this).removeClass('margin')
+  $(this).text('You clicked me!')
+})
+
+
+$('button').addClass('button')
+
+$('button').click(function () {
+  $(this).css('color', 'blue')
+  $(this).css('background-color', 'yellow')
+  $(this).css('font-family', 'comic sans ms')
+  $(this).text('You clicked me!')
+})
+
+$('button').html('Click Me!')
