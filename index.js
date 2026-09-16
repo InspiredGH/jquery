932e23faa3f59ffceb7cad240f4a5672ad90b8e1
InspiredGH <solomon.asante62@gmail.com>
2026-09-16 13:59:43 +0000

    Event listeners

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 $('h1').addClass('heading margin')
 
-$('h1').click(function () {
+$('h1').on('mouseover', function () {
   console.log(this)
   $(this).css('color', 'green')
   $(this).css('border', 'red 2px solid')
   $(this).removeClass('margin')
-  $(this).text('You clicked me!')
+  $(this).text('You hovered over me!')
 })
 
 $('button').addClass('button')
 
 $('button').click(function () {
   $(this).css('color', 'blue')
+  $('h1').css('color', 'red')
   $(this).css('background-color', 'yellow')
   $(this).css('font-family', 'comic sans ms')
   $(this).text('You clicked me!')
@@ -25,4 +26,12 @@ $('a').attr('href', 'https://www.yahoo.com')
 
 $('img').click(function () {
   console.log($('img').attr('src', 'images.jpg'))
+})
+
+$('#inputField').keypress(function (event) {
+  console.log(event.key)
+  $('h1').text(event.key)
+  if (event.key === 'Enter') {
+    console.log('You pressed the enter key!')
+  }
 })
